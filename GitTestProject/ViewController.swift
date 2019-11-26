4c8b1f7ee975cc066b0ef7ebfc69a7690b207677
Tayfun Bayindir <tayfunbayindir07@gmail.com>
2019-11-26 22:13:39 +0300

    first commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Tayfun Bayındır on 26.11.2019.
+//  Copyright © 2019 Tayfun Bayindir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+    }
+
+
+}
+
